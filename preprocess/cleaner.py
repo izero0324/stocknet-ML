3014e940bdfc7dc06a83640121cb68ae63f8ad5b
izero0324 <andrew1997tw24@gmail.com>
2024-02-26 23:56:00 +0000

    isolate csv_reader

--- preprocess/cleaner.py
+++ preprocess/cleaner.py
@@ -1,23 +1,15 @@
 import pandas as pd
-import sys
+from preprocess.csv_reader import read
 
 def clean_stock_data(stock_name):
     # Read the CSV file is named after the stock and located in 'price/raw/' folder
-    file_path = f'./price/raw/{stock_name}.csv'
     
-    # Load the dataset
-    df = pd.read_csv(file_path)
-    
+    file_name = stock_name+'.csv'
+    df = read(file_name)
     # Perform cleaning operations
     df_cleaned = df.dropna()
     
     # Correcting a specific data type
-    df_cleaned['date'] = pd.to_datetime(df_cleaned['date'])
+    df_cleaned['date'] = pd.to_datetime(df_cleaned['Date'])
     
     return df_cleaned
-
-if __name__ == "__main__":
-    # Argument passed via command line
-    stock_name = sys.argv[1]
-    cleaned_data = clean_stock_data(stock_name)
-    print(cleaned_data.head())
